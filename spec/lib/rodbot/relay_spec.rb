23604e9dc11072f9b8ba4642efded91af4156344
Sven Schwyn <sven.schwyn@bitcetera.com>
2023-09-06 02:50:35 +0200

    Streamline configs set in tests

--- spec/lib/rodbot/relay_spec.rb
+++ spec/lib/rodbot/relay_spec.rb
@@ -23,14 +23,14 @@ describe Rodbot::Relay do
 
   describe :bind do
     it "returns localhost and ports above 7200 by default" do
-      with '@config', Rodbot::Config.new("plugin :matrix\nplugin :slack"), on: Rodbot do
+      with '@config', Rodbot::Config.new("plugin :matrix; plugin :slack"), on: Rodbot do
         _(matrix.send(:bind)).must_equal ['localhost', 7201]
         _(slack.send(:bind)).must_equal ['localhost', 7202]
       end
     end
 
     it "returns localhost and ports about explicit port config" do
-      with '@config', Rodbot::Config.new("plugin :matrix\nplugin :slack\nport 8888"), on: Rodbot do
+      with '@config', Rodbot::Config.new("plugin :matrix; plugin :slack; port 8888"), on: Rodbot do
         _(matrix.send(:bind)).must_equal ['localhost', 8889]
         _(slack.send(:bind)).must_equal ['localhost', 8890]
       end
@@ -38,7 +38,7 @@ describe Rodbot::Relay do
 
     it "returns value of RODBOT_RELAY_HOST and ports above 7200" do
       with "ENV['RODBOT_RELAY_HOST']", '0.0.0.0' do
-        with '@config', Rodbot::Config.new("plugin :matrix\nplugin :slack"), on: Rodbot do
+        with '@config', Rodbot::Config.new("plugin :matrix; plugin :slack"), on: Rodbot do
           _(matrix.send(:bind)).must_equal ['0.0.0.0', 7201]
           _(slack.send(:bind)).must_equal ['0.0.0.0', 7202]
         end
@@ -49,7 +49,7 @@ describe Rodbot::Relay do
   describe :say do
     context "all relay extensions have say enabled" do
       with '@config', on: Rodbot do
-        Rodbot::Config.new("plugin(:matrix) { say true }\nplugin(:slack) { say true }")
+        Rodbot::Config.new("plugin(:matrix) { say true }; plugin(:slack) { say true }")
       end
 
       it "writes the message to all relay extensions" do
@@ -63,7 +63,7 @@ describe Rodbot::Relay do
 
     context "some relay extensions have say enabled" do
       with '@config', on: Rodbot do
-        Rodbot::Config.new("plugin(:matrix) { say true }\nplugin(:slack)")
+        Rodbot::Config.new("plugin(:matrix) { say true }; plugin(:slack)")
       end
 
       it "writes the message to all with say enabled" do
@@ -81,7 +81,7 @@ describe Rodbot::Relay do
 
     context "no relay extensions have say enabled" do
       with '@config', on: Rodbot do
-        Rodbot::Config.new("plugin(:matrix)\nplugin(:slack)")
+        Rodbot::Config.new("plugin(:matrix); plugin(:slack)")
       end
 
       it "writes no message to any relay extension" do
